05d55bfc448f3be3e3d0d0331d6f04f0d718d284
Sultan <linpark11197@gmail.com>
2023-04-09 17:40:19 +0600

    setup eslint

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-      alias: {
-        '@shared': path.resolve(__dirname, './src/shared'),
-      },
+    alias: {
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+    },
   },
   plugins: [react()],
-})
+});
