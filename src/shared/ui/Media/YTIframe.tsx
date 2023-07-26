89d548601dc225d77420896f4f25d59135452672
Sultan <linpark11197@gmail.com>
2023-07-26 23:45:48 +0600

    fix: верстка в ютуб iframe

--- src/shared/ui/Media/YTIframe.tsx
+++ src/shared/ui/Media/YTIframe.tsx
@@ -1,18 +1,22 @@
+import React from 'react';
+import './YTIframe.scss';
+
 type YTIframeProps = {
   src: string;
   height?: number | string;
   width?: number | string;
 };
 const YTIframe = (props: YTIframeProps) => {
-  const { src = '', height = 315, width = 560 } = props;
+  const { src = '', height = '', width = '' } = props;
   const vidId = src.split('v=')[1];
   if (!src) return <div></div>;
 
   return (
     <iframe
+      className='yt-iframe'
       width={width}
       height={height}
-      src={`https://www.youtube.com/embed/${vidId}`}
+      src={`https://www.youtube.com/embed/${vidId}?autoplay=1`}
       title='YouTube video player'
       frameborder='0'
       allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
