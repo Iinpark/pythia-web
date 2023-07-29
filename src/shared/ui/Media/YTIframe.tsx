e996e53794f2d58c4520ea0b7f092309d5e84cf7
Sultan <linpark11197@gmail.com>
2023-07-30 01:11:07 +0600

    fix: fullscreen

--- src/shared/ui/Media/YTIframe.tsx
+++ src/shared/ui/Media/YTIframe.tsx
@@ -19,7 +19,7 @@ const YTIframe = (props: YTIframeProps) => {
       src={`https://www.youtube.com/embed/${vidId}?autoplay=1&mute=1`}
       title='YouTube video player'
       frameborder='0'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen'
       allowfullscreen></iframe>
   );
 };
