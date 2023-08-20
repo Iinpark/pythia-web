067de43e3fe6ef5c70745b125fa7b4804c1d6d0b
Sultan <linpark11197@gmail.com>
2023-08-20 23:33:09 +0600

    images

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ export default {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com',
+        pathname: '/media/launcher_images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com',
+        pathname: '/media/launch_images/**',
+      },
+    ],
+  },
 };
--- src/shared/ui/LaunchCard/Index.tsx
+++ src/shared/ui/LaunchCard/Index.tsx
@@ -3,22 +3,33 @@ import './index.scss';
 import { LaunchData } from '@shared/interfaces';
 import { Typography } from '@mui/material';
 import Link from 'next/link';
+import Image from 'next/image';
 
-export const LaunchCard = ({ launchData }: { launchData: LaunchData }) => {
+export const LaunchCard = async ({
+  launchData,
+}: {
+  launchData: LaunchData;
+}) => {
   const date = new Date(launchData.t0).toLocaleString('ru');
 
   return (
-    <article
-      className='launch-card'
-      style={{ backgroundImage: `url(${launchData.image})` }}>
-      <Link href={`/launch/${launchData.id}`}>
-        <Typography className='launch-card_title' variant='h1'>
-          {launchData.name}
+    <article className='launch-card'>
+      <Image
+        className='launch-card_image'
+        fill
+        src={launchData.image}
+        alt={launchData.name}
+      />
+      <div className='launch-card_content'>
+        <Link href={`/launch/${launchData.id}`}>
+          <Typography className='launch-card_title' variant='h1'>
+            {launchData.name}
+          </Typography>
+        </Link>
+        <Typography className='launch-card_subtitle' variant='h4'>
+          {date}
         </Typography>
-      </Link>
-      <Typography className='launch-card_subtitle' variant='h4'>
-        {date}
-      </Typography>
+      </div>
     </article>
   );
 };
