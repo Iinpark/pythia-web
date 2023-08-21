422da163fa9fc197b4a1b68ecd9e977504d51702
Sultan <linpark11197@gmail.com>
2023-08-22 00:05:41 +0600

    metadata on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import LaunchesGallery from '@widgets/LaunchesGallery/Index.tsx';
 import './page.scss';
-import Head from 'next/head';
+import type { Metadata } from 'next';
 import fetchUpcomingLaunches from '@shared/api/queryFns/fetchUpcomingLaunches.ts';
 
+export const metadata: Metadata = {
+  title: 'Pythia | Schedule of rocket launches ',
+  description:
+    'Pythia is a schedule of rocket launches. Here you can find information about upcoming launches, as well as about some past ones.',
+};
 async function App() {
   const launches = await fetchUpcomingLaunches();
   return (
     <main className='upcoming-launches'>
-      <Head>
-        <title>Pythia | Schedule of rocket launches </title>
-
-        <meta
-          name='description'
-          content='Pythia is a schedule of rocket launches. Here you can find information about upcoming launches, as well as about some past ones.'
-          key='desc'
-        />
-      </Head>
-
       <h1 className='page-header'>Upcoming Launches</h1>
       <LaunchesGallery launches={launches} />
     </main>
