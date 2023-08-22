5368515be6903017a5c6292947c989b7991a023a
Sultan <linpark11197@gmail.com>
2023-08-22 23:06:58 +0600

    fix: youtube id parsing

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,42 @@
+export const toDisplayableDate = (date) => Date(date).toLocaleString();
+
+type StringAssemblerTokens = {
+  [key: string]: string;
+};
+type StringAssemblerTemplate = string;
+
+/**
+ * Assembles a string from a template and a set of tokens.
+ *
+ * @param {StringAssemblerTokens} tokens - The tokens to be used in the template.
+ * @param {StringAssemblerTemplate} template - The template to be used to assemble the string.
+ *
+ * @example ```javascript
+ *  stringAssembler({ name: 'John' }, 'Hello, {name}!') // 'Hello, John!
+ * ```
+ */
+export const stringAssembler = (
+  tokens: StringAssemblerTokens,
+  template: StringAssemblerTemplate
+) => {
+  return Object.keys(tokens).reduce((acc, key) => {
+    const tokenPattern = new RegExp(`{${key}}`, 'g');
+    return acc.replace(tokenPattern, tokens[key]);
+  }, template);
+};
+
+export const isYoutubeUrl = (url) => {
+  const youtubeUrlPattern =
+    /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/g;
+  return youtubeUrlPattern.test(url);
+};
+
+export const getYouTubeVideoId = (url) => {
+  const youtubeUrlPattern =
+    /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/g;
+  const youtubeVideoIdPattern = /(?<=v=|\/)([a-zA-Z0-9_-]{11})(?=&|$|\/)/;
+  if (youtubeUrlPattern.test(url)) {
+    return url.match(youtubeVideoIdPattern)[0];
+  }
+  return null;
+};
